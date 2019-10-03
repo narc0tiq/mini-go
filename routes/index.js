de0e208a58de550b68f3cc3b37a58205ee28c7cf
Octav Sandulescu <github@narc.ro>
2019-10-03 21:09:44 +0300

    Simplify. A lot.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(`Hello, world!`);
+  //res.redirect(307, 'https://www.google.com/');
 });
+
+router.all('/:keyword', function(req, res, next) {
+  res.send(`It's a ${req.params.keyword} world!`)
+})
 
 module.exports = router;
