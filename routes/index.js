fccae66d0b4bf4e32563109c8a150b3f1e6188e7
Octav Sandulescu <github@narc.ro>
2019-10-03 21:51:53 +0300

    Add SQLite and do query for redirects

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const sqlite = require('sqlite3').verbose();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(`Hello, world!`);
-  //res.redirect(307, 'https://www.google.com/');
+  res.send(`Hello, world! ${req.app.get('env')}`);
 });
 
 router.all('/:keyword', function(req, res, next) {
-  res.send(`It's a ${req.params.keyword} world!`)
+  const db = new sqlite.Database('mini-go.sqlite3.db', sqlite.OPEN_READWRITE);
+  const query = 'SELECT * FROM redirects WHERE keyword = ?';
+
+  db.get(query, [req.params.keyword], (err, row) => {
+    if(err) { throw err; }
+
+    if(row) {
+      res.send(`I'd send you to ${row.url}`);
+    } else {
+      res.statusCode = 404;
+      res.send(`No redirect for '${req.params.keyword}'!`);
+    }
+  })
+  //res.redirect(307, 'https://www.google.com/');
 })
 
 module.exports = router;
