d19e4493a1be5f77759fd1ad8dbf5e6accfd6d96
Octav Sandulescu <octav.sandulescu@sparkware.ro>
2019-10-04 17:37:53 +0300

    Handle failing to open DB

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,15 @@ router.get('/', function(req, res, next) {
   res.send(`Hello, world! ${req.app.get('env')}`);
 });
 
+function fail(res, code, message) {
+  res.statusCode = code;
+  res.send(message);
+}
+
 router.all('/:keyword', function(req, res, next) {
-  const db = new sqlite.Database('mini-go.sqlite3.db', sqlite.OPEN_READWRITE);
+  const db = new sqlite.Database('mini-go.sqlite3.db', sqlite.OPEN_READWRITE, (err) => {
+    if(err) { fail(res, 500, `Failed to connect: ${err.message}`); }
+  });
   const query = 'SELECT * FROM redirects WHERE keyword = ?';
 
   db.get(query, [req.params.keyword], (err, row) => {
@@ -17,8 +24,7 @@ router.all('/:keyword', function(req, res, next) {
     if(row) {
       res.send(`I'd send you to ${row.url}`);
     } else {
-      res.statusCode = 404;
-      res.send(`No redirect for '${req.params.keyword}'!`);
+      fail(res, 404, `No redirect for '${req.params.keyword}'!`);
     }
   })
   //res.redirect(307, 'https://www.google.com/');
